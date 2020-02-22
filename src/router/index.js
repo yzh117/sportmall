0f99853ba084bcd9c6a90b5828e0946944ee3e23
Your Name <email@example.com>
2020-02-22 18:47:22 +0800

    sportmall

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from  'vue-router'
+
+const Home = () => import()
+
+//1.安装插件
+Vue.use(VueRouter)
+//2.创建router
+const routes = [
+
+]
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+//3.导出
+export default router
